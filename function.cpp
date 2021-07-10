704df155567d271b0d21dce0564b1c383158b96b
AkshatMishra01 <59910316+AkshatMishra01@users.noreply.github.com>
2021-07-10 09:44:47 +0530

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -15,3 +15,23 @@ int add(int x, int y){
     int z = x+y;
     return z;
 }
+
+// Call by pointers
+
+#include<iostream>
+using namespace std;
+
+int add(int *x,int *y);
+
+int main(){
+    int a= 10;
+    int b = 20;
+    add(&a,&b);
+    cout<<"Addition gives:"<<add(&a,&b)<<endl;
+
+}
+
+int add(int *x, int *y){
+    int z = *x+*y;
+    return z;
+}
